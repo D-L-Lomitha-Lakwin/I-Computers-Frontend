fb0249cec70a6ae9c7096ef67e1fecf724440501
Lomitha Lakwin <lomithalakwin@gmail.com>
2025-10-01 11:19:24 +0530

    Day 10 alignment changed 📝

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,23 @@ function App() {
   return (
     <>
 
-      <ProductCard name="Laptop" price="100,000" image="https://tse3.mm.bing.net/th/id/OIP.L12A9cgd7dCHFZFd8BTPKwHaHZ?rs=1&pid=ImgDetMain&o=7&rm=3" />
+      <ProductCard 
+        name="Laptop"
+        price="100,000" 
+        image="https://tse3.mm.bing.net/th/id/OIP.L12A9cgd7dCHFZFd8BTPKwHaHZ?rs=1&pid=ImgDetMain&o=7&rm=3" 
+      />
 
-      <ProductCard name="Phone" price="70,000" image="https://tse2.mm.bing.net/th/id/OIP.BQhwKHwVYvkFSiV6jwk_RAHaHj?rs=1&pid=ImgDetMain&o=7&rm=3"/>
+      <ProductCard 
+        name="Phone" 
+        price="70,000" 
+        image="https://tse2.mm.bing.net/th/id/OIP.BQhwKHwVYvkFSiV6jwk_RAHaHj?rs=1&pid=ImgDetMain&o=7&rm=3"
+      />
 
-      <ProductCard name="Watch" price="5,000" image="https://tse2.mm.bing.net/th/id/OIP.8Js70CPlVBwuwX8HTrJBfAHaG6?rs=1&pid=ImgDetMain&o=7&rm=3"/>
+      <ProductCard 
+        name="Watch" 
+        price="5,000" 
+        image="https://tse2.mm.bing.net/th/id/OIP.8Js70CPlVBwuwX8HTrJBfAHaG6?rs=1&pid=ImgDetMain&o=7&rm=3"
+      />
 
 
     </>
